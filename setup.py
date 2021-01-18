ba4279124bb47c1c7f594dd065ee07e89b962d4d
Leo Gallucci <leo.gallucci@zalando.de>
2021-01-18 15:24:51 +0100

    [ZAL] unnecessary pinned dependencies

--- setup.py
+++ setup.py
@@ -385,7 +385,7 @@ def do_setup():
             'text-unidecode==1.2',
             'typing;python_version<"3.5"',
             'thrift>=0.9.2',
-            'tzlocal>=1.4,<2.0.0',
+            'tzlocal>=1.4',
             'unicodecsv>=0.14.1',
             'zope.deprecation>=4.0, <5.0',
         ],
